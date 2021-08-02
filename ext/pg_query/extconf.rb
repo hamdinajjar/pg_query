75f874cc27241a93486fb8ad66227d3008c0194d
hamdinajjar <hamdi@adomik.com>
2021-08-02 15:36:28 +0200

    add c99 flag

--- ext/pg_query/extconf.rb
+++ ext/pg_query/extconf.rb
@@ -7,7 +7,7 @@ require 'pathname'
 
 $objs = Dir.glob(File.join(__dir__, '*.c')).map { |f| Pathname.new(f).sub_ext('.o').to_s }
 
-$CFLAGS << " -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -g"
+$CFLAGS << " -O3 -Wall -fno-strict-aliasing -fwrapv -fstack-protector -Wno-unused-function -Wno-unused-variable -g -std=c99"
 
 $INCFLAGS = "-I#{File.join(__dir__, 'include')} " + $INCFLAGS
 
